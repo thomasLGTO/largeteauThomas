0423f17b57aa0bd04dbaed8f12a46675c8681386
thomasLGTO <thomaslargeteau87@gmail.com>
2020-02-19 11:02:33 +0100

    gestion des messages d'erreur pour chaque champ du formulaire

--- src/Entity/Films.php
+++ src/Entity/Films.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\FilmsRepository")
@@ -18,21 +19,42 @@ class Films
 
     /**
      * @ORM\Column(type="string", length=255)
+     *  * @Assert\Length(
+     *      min = 2,
+     *      max =100,
+     *      minMessage = "Le titre du film est trop court",
+     *      maxMessage = "Le titre du film est trop long",
+     * )
      */
     private $titre;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * * @Assert\Url(
+     * message="Ce n'est pas une url")
+     *      
      */
     private $affiche;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * * @Assert\Length(
+     *      min = 2,
+     *      max =100,
+     *      minMessage = "Le nom du réalisateur est trop court",
+     *      maxMessage = "Le nom du réalisateur est trop long",
+     * )
      */
     private $realisateur;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * * * @Assert\Length(
+     *      min = 2,
+     *      max =100,
+     *      minMessage = "Le nom de la récompense est trop courte",
+     *      maxMessage = "Le nom de la récompense est trop longue",
+     * )
      */
     private $recompense;
 
@@ -42,7 +64,13 @@ class Films
     private $sorti;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=1000)
+     * * * * @Assert\Length(
+     *      min = 15,
+     *      max =1000,
+     *      minMessage = "Le synopsis est trop court",
+     *      maxMessage = "Le synopsis est trop long",
+     * )
      */
     private $synopsis;
 
--- src/Form/FilmsType.php
+++ src/Form/FilmsType.php
@@ -6,6 +6,7 @@ use App\Entity\Films;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,16 +15,38 @@ class FilmsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('affiche')
-            ->add('realisateur')
-            ->add('recompense')
+            ->add('titre',TextType::class, [
+                'label' => 'Saisir le titre du Film',
+                'required'=>false,
+                'attr' => [
+                    'placeholder' => 'entrez-le ici !'
+            ]])
+            ->add('affiche',TextType::class, [
+                'label' => 'L\'affiche du film',
+                'required'=>false,
+                'attr' => [
+                    'placeholder' => 'entrer l\'url de l\'affiche ici  !'
+            ]])
+            ->add('realisateur',TextType::class, [
+                'label' => 'Saisir le réalisateur',
+                'required'=>false,
+                'attr' => [
+                    'placeholder' => 'juste ici !'
+            ]])
+            ->add('recompense',TextType::class, [
+                'label' => 'Récompense pour le film',
+                'required'=>false,
+                'attr' => [
+                    'placeholder' => 'ex : Palme d\'or'
+            ]])
             ->add('sorti',DateType::class, [
                 'years' => range('1900', date('2020')),
                 'format' => 'dd MM yyyy',
             ])
             ->add('synopsis',TextareaType::class, [
-                
+                'attr' => [
+                    'placeholder' => 'Bla bla bla'
+            ]
             ])
         ;
     }
